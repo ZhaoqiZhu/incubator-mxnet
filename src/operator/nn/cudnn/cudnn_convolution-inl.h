b661e0aa304937bcba48aa3bdb8d6576f148f871
zha0q1 <zhaoqizh@usc.edu>
2019-07-03 17:38:47 -0700

    a

--- src/operator/nn/cudnn/cudnn_convolution-inl.h
+++ src/operator/nn/cudnn/cudnn_convolution-inl.h
@@ -138,7 +138,7 @@ class CuDNNConvolutionOp {
     size_t workspace_size = TensorSizeBytes(workspace);
     
     TensorInspector ti(workspace);
-    workspace.print_string();    
+    ti.print_string();    
 
     // I/O's should have 2 more dims than the kernel dim
     DType *data_ptr = GetNdPtr(in_data[conv::kData], param_.kernel.ndim() + 2, s);
